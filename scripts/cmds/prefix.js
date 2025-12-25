544d5d822e3837c9c0b4715a47ee3e15198bbef7
Melikuate <melikuate7002@gmail.com>
2025-12-25 17:02:52 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -4,7 +4,7 @@ const { utils } = global;
 module.exports = {
   config: {
     name: "prefix",
-    aliases: ["🌚"],
+    aliases: ["🛌"],
     version: "1.3",
     author: "messie osango",
     countDown: 5,
